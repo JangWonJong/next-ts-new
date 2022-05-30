0c4d471b67600c87602becdcf044831aa68444d2
JangWonJong <jwj9922@naver.com>
2022-05-30 20:13:06 +0900

    docker

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,11 +1,6 @@
-import {
-    TypedUseSelectorHook,
-    useDispatch,
-    useSelector,
-    Provider
-} from 'react-redux'
+import {  TypedUseSelectorHook,  useDispatch,  useSelector } from 'react-redux'
 
-import type {RootState, AppDispatch } from '@/modules/store'
+import type {AppState, AppDispatch } from '@/modules/store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
